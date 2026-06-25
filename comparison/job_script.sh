8bc7e8621b14feaab657e0d93b3bf0e79e68fdaa
Manuel Hinkel <hinkel06@ads.uni-passau.de>
2026-06-25 09:54:17 +0200

    execution changes

--- comparison/job_script.sh
+++ comparison/job_script.sh
@@ -2,7 +2,5 @@
 
 DATA_DIR="$1"
 
-files=("$DATA_DIR"/*)
 
-i=$((SLURM_ARRAY_TASK_ID-1))
-java -cp ./../target/classes de.ControlClosure.Evaluation.ComparisonRunner "${files[i]}" "100"
+java -cp ./../target/classes de.ControlClosure.Evaluation.ComparisonRunner "$DATA_DIR" "20"
--- comparison/main.sh
+++ comparison/main.sh
@@ -6,11 +6,10 @@ INSTANCE_DIR="instances/${C_SUB_DIR}"
 
 echo "$INSTANCE_DIR"
 
-OUT_DIR="instance_results/${C_SUB_DIR}"
+OUT_FILE="instance_results/${C_SUB_DIR}.csv"
 
-echo "$OUT_DIR"
+echo "$OUT_FILE"
 
 ./compile.sh
 
-n=$(ls $INSTANCE_DIR -1 | wc -l)
-sbatch --array=1-$n -n 1 -N 1 --exclusive=user -p infosun --constraint=chimaira --output="./${OUT_DIR}/slurm-%A_%a.out" --mem=10G  ./job_script.sh "$INSTANCE_DIR"
+sbatch --array=1-1 -n 1 -N 1 --exclusive -p infosun --constraint=chimaira --output="./${OUT_FILE}" --mem=32G  ./job_script.sh "$INSTANCE_DIR"
